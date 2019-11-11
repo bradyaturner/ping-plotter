c0d7569a0c1bb2e7ccf64d25841bbf448ff2e99a
Brady Turner <bradyaturner@gmail.com>
2019-11-11 12:03:37 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Sample Usage
+
+ping 8.8.8.8 -D | ./ping_to_csv.rb
---
+++ ping_to_csv.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+
+
+ARGF.each do |line|
+  next if line.match(/^PING/)
+  data = line.split
+  ts = data.first[/\[(.*?)\]/,1]
+  ptime = data[-2].split("=").last
+  puts ts + "," + ptime
+end
